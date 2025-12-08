1e0161fabdd06362094698521c4fd16c69ba1d7f
Vishal Vusirikala <vvusirik@gmail.com>
2025-12-07 19:51:46 -0500

    use react query for equity heatmap

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 import EquityGrid from "../components/EquityGrid";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 export default function Home() {
-    return <EquityGrid></EquityGrid>;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <EquityGrid />
+        </QueryClientProvider>
+    );
 }
